eb2cb14848e8453ef72dffc8ba5de0f659a66bb8
Bhaumik <bhaumik5555@gmial.com>
2023-09-11 09:21:58 -0400

    Added Slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import SwiftSlider from 'react-swift-slider'
+import image1 from '../../assets/Images/Food/1.avif';
+import image2 from '../../assets/Images/Food/2.jpg';
+import image3 from '../../assets/Images/Food/3.jpg';
+import image4 from '../../assets/Images/Food/4.webp';
+import image5 from '../../assets/Images/Food/5.jpg';
+import image6 from '../../assets/Images/Food/6.jpg';
+
+
+const data =  [
+  {'id':'1','src':image1},
+  {'id':'2','src':image2},
+  {'id':'3','src':image3},
+  {'id':'4','src':image4},
+  {'id':'5','src':image5},
+  {'id':'6','src':image6},
+];
 
 const Slider = () => {
   return (
     <>
-    <div>Slider</div>
-    <SwiftSlider/>
+    <SwiftSlider data={data} enableNextAndPrev={false}/>
     </>
   )
 }
